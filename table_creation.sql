67d9fa06b6158b8e9b2049a2d7e736fe4ca37da1
Simran Gaheer <127239843+skgaheer@users.noreply.github.com>
2026-04-14 19:59:18 -0700

    Update table_creation.sql

--- table_creation.sql
+++ table_creation.sql
@@ -5,7 +5,7 @@
 -- to create the empty table in database: 
 CREATE TABLE tickets (
     ticket_number VARCHAR(20) PRIMARY KEY,
-    short_discription TEXT,
+    short_description TEXT,
     assignment_group TEXT,
     comments TEXT,
     date_created TIMESTAMP,
@@ -18,7 +18,7 @@ CREATE TABLE tickets (
 
 psql -U sgaheer -h localhost -p 5432 -d servicenow -c "CREATE TABLE tickets (
     ticket_number VARCHAR(20) PRIMARY KEY,
-    short_discription TEXT,
+    short_description TEXT,
     assignment_group TEXT,
     comments TEXT,
     date_created TIMESTAMP,
